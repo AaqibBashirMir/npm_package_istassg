70624bff85cbb8174f76348217203201e6de9ded
AaqibBashirMir <dazzlingmustakeen@gmail.com>
2024-05-06 20:36:21 +0530

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const add=(a,b)=>a+b;
+const sub=(a,b)=>a-b;
+const multiply=(a,b)=>a*b;
+const divide=(a,b)=>a/b;
+
+
+const calculator={
+    add,
+    sub,
+    multiply,
+    divide,
+};
+
+
+module.exports=calculator
